0d05c87b536a01f8c9eacbf1caf6a7550c691111
Kosta Harlan <kosta@kostaharlan.net>
2016-04-06 11:30:53 -0400

    Issue #940: More Markdown rules

--- .markdown.style.rb
+++ .markdown.style.rb
@@ -3,8 +3,25 @@
 rule "MD001" # Header levels should only increment by one level at a time
 rule "MD002" # First header should be a h1 header
 rule "MD003" # Header style
+rule "MD004" # Unordered list style
+rule "MD005" # Inconsistent indentation for list items at the same level
+rule "MD006" # Consider starting bulleted lists at the beginning of the line
+rule "MD007"
 rule "MD009" # No trailing spaces.
 rule "MD010" # No hard tabs.
 rule "MD011" # Reversed link syntax.
 rule "MD012" # No consecutive blank lines.
+rule "MD014" # Dollar signs used before commands without showing output
+rule "MD018" # No space after hash on atx style header
+rule "MD019" # Multiple spaces after hash on atx style header
+rule "MD021" # Multiple spaces inside hashes on closed atx style header
+rule "MD022" # Headers should be surrounded by blank lines
+rule "MD023" # Headers must start at the beginning of the line
+rule "MD024" # Multiple headers with the same content
+rule "MD025" # Multiple top level headers in the same document
+rule "MD026" # Trailing punctuation in header
+rule "MD027" # Multiple spaces after blockquote symbol
+rule "MD028" # Blank line inside blockquote
+rule "MD030" # Spaces after list markers
 rule "MD031" # Surround fenced code blocks with blank lines.
+rule "MD032" # Lists should be surrounded by blank lines
