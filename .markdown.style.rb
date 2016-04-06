38569c0bf1eed5e2611bb8e8b1be43264d7a4e42
Kosta Harlan <kosta@kostaharlan.net>
2016-04-06 10:57:24 -0400

    Issue #940: Tighten up MDL, add status image

--- .markdown.style.rb
+++ .markdown.style.rb
@@ -1,5 +1,8 @@
 # Select and configure Markdown Lint rules.
 
+rule "MD001" # Header levels should only increment by one level at a time
+rule "MD002" # First header should be a h1 header
+rule "MD003" # Header style
 rule "MD009" # No trailing spaces.
 rule "MD010" # No hard tabs.
 rule "MD011" # Reversed link syntax.
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/savaslabs/developer-docs.svg?branch=master)](https://travis-ci.org/savaslabs/developer-docs)
+
 # Write the docs
 
 ## Building the project locally
